fbf5e047cff398684e479739d46bc3622722e0d0
Edward Hong <edward.xc.hong@gmail.com>
2018-09-09 19:43:47 -0500

    reimplement FCFS with queues and code refactor

--- operating-systems/pgm/pgm1/scheduling.cpp
+++ operating-systems/pgm/pgm1/scheduling.cpp
@@ -6,41 +6,28 @@
 #include "Process.h"
 
 // * Constants
-static const unsigned int NUM_SIMULATIONS = 1;
+static const unsigned int NUM_SIMULATIONS = 1;  // TODO: REMEMBER TO CHANGE THIS BACK TO 1000
 static const unsigned int NUM_PROCESSES = 20;
-static const unsigned int NUM_CYCLES = 200;
-
-// * Variable Declarations
-//int simulations[NUM_SIMULATIONS]; // not sure if this is actually necessary but TODO: REMEMBER TO CHANGE THIS BACK TO 1000
-int processes[NUM_PROCESSES];
-
-
-std::queue<Process> processQueue;
-
-
-// Represents time in clock cycles
-// Array index represents the cycle at which a process finished
-// Value in the array represents process id, [1-20], with -1 representing no-op
-int cycles[NUM_CYCLES];
 
 // SPN Comparator
-struct CompSPN{
+struct comp_spn {
     bool operator()(const Process& a, const Process& b){
         return a.serviceTime > b.serviceTime;
     }
 };
 
 // SPN Comparator
-struct CompSRT{
+struct comp_srt {
     bool operator()(const Process& a, const Process& b){
         return a.remainingTime > b.remainingTime;
     }
 };
 
-// * Function Declarations
-void init();
-void fcfs();
-void showTimeline();
+// Queue Declarations for the four scheduling algorithms
+std::queue<Process> q_fcfs;
+std::queue<Process> q_rr;
+std::priority_queue<Process, std::vector<Process>, comp_spn> pq_spn;
+std::priority_queue<Process, std::vector<Process>, comp_srt> pq_srt;
 
 template<typename T> void printQueue(T& q) {
     while(!q.empty()) {
@@ -50,105 +37,120 @@ template<typename T> void printQueue(T& q) {
     std::cout << '\n';
 }
 
-void printProcessQueue() {
-    while(!processQueue.empty()) {
-        std::cout << processQueue.front().pid << " ";
-        processQueue.pop();
-    }
-    std::cout << '\n';
-}
-
-int main()
-{
-    init();
-
-    for (unsigned int i = 0; i < NUM_SIMULATIONS; ++i)
-    {
-        std::cout << "> Simulation " << i << " running..." << std::endl;
-
-        std::priority_queue<Process, std::vector<Process>, CompSPN> SPNQueue;
-        std::priority_queue<Process, std::vector<Process>, CompSRT> SRTQueue;
-
-
-        for(unsigned int pid = 0; pid < NUM_PROCESSES; ++pid)
-        {
-            // Assigning a random Service Time between 1 and 10
-            int serviceTime = rand() % 10 + 1;
-            Process p = Process(pid, serviceTime);
-            
-            processQueue.push(p);
-            SPNQueue.push(p);
-            SRTQueue.push(p);
-            
-            std::cout << "  Process " << pid << " ST = " << serviceTime << std::endl;
-        }
-
-        // FCFS Simulation
-        //fcfs();
-        //showTimeline();
-
-        std::cout << "PROCESS QUEUE" << std::endl;
-        printProcessQueue();
-        
-        std::cout << "SPN QUEUE" << std::endl;
-        printQueue(SPNQueue);
-
-        std::cout << "SRT QUEUE" << std::endl;
-        printQueue(SRTQueue);
-
-
-    }
-
-	return 0;
-}
-
-// TODO: ARRIVAL TIMES - right now this essentially assumes all processes arrive at time 0
-void fcfs()
-{
-    std::cout << "- FCFS:" << std::endl;
+// Prints Program Name and Author to the console
+// and seeds the Random Number Generator
+void init() {
+	std::cout << "--- Program 1 : Uniprocessor Scheduling Benchmarks" << std::endl;
+    std::cout << "--- Author    : Edward Hong" << std::endl;
     
-    int currentTime = 0;
+    // Seed our RNG
+    srand(time(NULL));
+}
 
-    // Executing each process in order
-    for(unsigned int p = 0; p < NUM_PROCESSES; ++p)
-    {
-        // Ensures the process has arrived by inserting no-op cycles
-        // Arrival time is twice the process index, p
-        int noops = (2*p) - currentTime; 
-        for(int n = 0; n < noops; ++n)
-        {
-            cycles[currentTime] = -1;
-            ++currentTime;
+void fcfs() {
+    std::cout << "- FCFS:" << std::endl;
+
+    // Executing each process in order of FCFS
+    while(!q_fcfs.empty()) {
+        Process currentProcess = q_fcfs.front();
+
+        // Check that the process has arrived
+        if(currentProcess.arrivalTime <= Process::currentTime) {
+            // Executing a single process to completion
+            for(unsigned int t = currentProcess.remainingTime; t > 0; --t) {
+                ++Process::currentTime;
+                currentProcess.process(1);
+            }
+            
+            std::cout << "PID:" << currentProcess.pid << " " << std::endl;
+            std::cout << "ST:" << currentProcess.serviceTime << " " << std::endl;
+            std::cout << "FT:" << currentProcess.finishTime << " " << std::endl;
+            std::cout << "T:" << currentProcess.turnaround << " " << std::endl;
+            std::cout << "RT:" << currentProcess.relativeTurnaround << " " << std::endl;
+            std::cout << std::endl;
+
+            q_fcfs.pop();
+        } else {
+            ++Process::currentTime;
+        }
+    }
+}
+
+void rr() {
+    std::cout << "- RR:" << std::endl;
+
+    // Executing each process in order of RR
+    while(!q_rr.empty()) {
+        Process currentProcess = q_rr.front();
+
+        // Check that the process has arrived
+        if(currentProcess.arrivalTime <= Process::currentTime) {
+            // Executing a single process for one cycle (q = 1)
+            ++Process::currentTime;
+            currentProcess.process(1);
+            q_rr.pop();
+
+            // Place process back into the queue if it hasn't finished executing
+            if(currentProcess.remainingTime > 0) {
+                q_rr.push(currentProcess);
+            }
+
+        } else {
+            ++Process::currentTime;
         }
 
-        // Executes a single process to completion
-        for(unsigned int i = processes[p]; i > 0; --i)
-        {
-            cycles[currentTime] = p;
-            ++currentTime;
-        }
+        std::cout << "PID:" << currentProcess.pid << " " << std::endl;
+        std::cout << "ST:" << currentProcess.serviceTime << " " << std::endl;
+        std::cout << "FT:" << currentProcess.finishTime << " " << std::endl;
+        std::cout << "T:" << currentProcess.turnaround << " " << std::endl;
+        std::cout << "RT:" << currentProcess.relativeTurnaround << " " << std::endl;
+        std::cout << std::endl;
     }
 }
 
 // For Round-Robin the currently executing process (which was interrupted) gets placed in the BACK of the queue
 // Consider using a queue to hold the processes as they arrive, then iterate through time to perform scheduling
 
-void showTimeline() 
-{
-    for(unsigned int c = 0; c < NUM_CYCLES; ++c)
-    {
-        std::cout << cycles[c] << " ";
-    }
-    std::cout << std::endl;
-}
-
-// Prints Program Name and Author to the console
-// and seeds the Random Number Generator
-void init()
-{
-	std::cout << "--- Program 1 : Uniprocessor Scheduling Benchmarks" << std::endl;
-    std::cout << "--- Author    : Edward Hong" << std::endl;
+int main() {
     
-    // Seed our RNG
-    srand(time(NULL));
+    init();
+
+    for (unsigned int i = 0; i < NUM_SIMULATIONS; ++i)
+    {
+        std::cout << "> Simulation " << i << " running..." << std::endl;
+
+
+        // Populating the queues with processes of random service time
+        for(unsigned int pid = 0; pid < NUM_PROCESSES; ++pid)
+        {
+            // Assigning a random Service Time between 1 and 10
+            int serviceTime = rand() % 10 + 1;
+            Process p = Process(pid, serviceTime);
+            
+            //debug
+            //Process p = Process(pid, 1);
+
+
+            q_fcfs.push(p);
+            q_rr.push(p);
+            pq_spn.push(p);
+            pq_srt.push(p);
+            
+            std::cout << "  Process " << pid << " ST = " << serviceTime << std::endl;
+        }
+
+        fcfs();     // Simulate FCFS scheduling algorithm
+        
+        rr();     // Simulate RR scheduling algorithm
+
+        std::cout << "SPN QUEUE" << std::endl;
+        printQueue(pq_spn);
+
+        std::cout << "SRT QUEUE" << std::endl;
+        printQueue(pq_srt);
+
+
+    }
+
+	return 0;
 }
