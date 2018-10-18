eb2dc1a05c8c8cd25ce5c155175ecbe6c82be7f6
Edward Hong <edward.xc.hong@gmail.com>
2018-10-18 16:55:40 -0500

    init program 2

---
+++ operating-systems/pgm/pgm2/main.cpp
@@ -0,0 +1,7 @@
+#include <stdlib.h>
+#include <iostream>
+
+int main() {
+	std::cout << "--- Program 2 : Producer Consumer" << std::endl;
+    std::cout << "--- Author    : Edward Hong" << std::endl;
+}
