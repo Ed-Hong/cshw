f2ae89e0bc1e0dde3d5ac69addf51a88b753f136
Edward Hong <edward.xc.hong@gmail.com>
2019-07-07 04:41:40 -0500

    update config parser

--- advanced-os/project1/src/Node.java
+++ advanced-os/project1/src/Node.java
@@ -73,23 +73,80 @@ public class Node {
 	}
 
 	private static void config(Scanner cfg, String currentHostname) {
+		int[] params = new int[6];
+
 		System.out.println("Configuring this node...");
 		Scanner line = null;
 
 		// Read each line
+		int lineNum = 0;
 		while (cfg.hasNextLine()) {
 			String lineStr = cfg.nextLine();
 			line = new Scanner(lineStr);
+			boolean isValidLine = true;
+			int tokenNum = 0;
 
 			// Read each token within a line
 			while(line.hasNext()) {
 				String token = line.next();
-				if (token.equals("#")) break; 	// Skip over comments
-				System.out.println(token);
+
+				// Skip over non-valid lines. Valid lines begin with unsigned int
+				if (tokenNum == 0 && !isUnsignedInt(token)) {
+					isValidLine = false;
+					break;
+				}
+
+				// Skip over comments
+				if (token.equals("#")) break;
+				
+				//debug
+				System.out.println(lineNum);
+
+				// Assign Global Parameters
+				if (lineNum == 0) {
+					System.out.println("token = " + token);
+					params[tokenNum] = Integer.parseInt(token);
+				}
+
+				tokenNum++;
+			}
+
+			// Make sure the line is valid and has at least 1 token
+			if(isValidLine && tokenNum > 0) {
+				lineNum++;
 			}
 		}
 
 		if(cfg != null) cfg.close();
 		if(line != null) line.close();
+
+		// Re-assigning Global Parameters to named constants
+		aliasGlobalParams(params);
+	}
+
+	private static boolean isUnsignedInt(String str) {
+		try {
+			int i = Integer.parseInt(str);
+			return i >= 0;
+		} catch (NumberFormatException | NullPointerException e) {
+			return false;
+		}
+	}
+
+	private static void aliasGlobalParams(int[] params) {
+		NUM_NODES 		= params[0];
+		MIN_PER_ACTIVE 	= params[1];
+		MAX_PER_ACTIVE 	= params[2];
+		MIN_SEND_DELAY 	= params[3];
+		SNAPSHOT_DELAY 	= params[4];
+		MAX_NUMBER 		= params[5];
+
+		System.out.println("NUM_NODES = " + NUM_NODES);
+		System.out.println("MIN_PER_ACTIVE = " + MIN_PER_ACTIVE);
+		System.out.println("MAX_PER_ACTIVE = " + MAX_PER_ACTIVE);
+		System.out.println("MIN_SEND_DELAY = " + MIN_SEND_DELAY);
+		System.out.println("SNAPSHOT_DELAY = " + SNAPSHOT_DELAY);
+		System.out.println("MAX_NUMBER = " + MAX_NUMBER);
+
 	}
 }
