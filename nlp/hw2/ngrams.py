234ecc282f24dc64a61bdab6e9d157349ebaa0b0
Edward Hong <edward.xc.hong@gmail.com>
2020-02-23 18:48:30 -0600

    hw2: generate unigram model

--- nlp/hw2/ngrams.py
+++ nlp/hw2/ngrams.py
@@ -7,32 +7,68 @@ import sys, getopt
 #   +1 smoothing
 #   Good-Touring smoothing
 
-def main(argv):
-    n = 1
-    smoothing = 'none'
+tokenCount = 0
 
+counts = {}
+unigram = {}
+
+def buildCounts():
+    lines = open("test.txt", "r")
+    for line in lines:
+        word_tags = line.split()
+        for word_tag in word_tags:
+            pair = word_tag.split('_')
+            word = pair[0]
+            count(word)
+            global tokenCount
+            tokenCount += 1
+
+def count(word):
+    if word not in counts.keys():
+        counts[word] = 1
+    else:
+        counts[word] += 1
+
+def buildUnigram():
+    uniqueWords = len(counts)
+    for word,count in counts.items():
+        unigram[word] = count/tokenCount
+        
+
+def main(argv):
     if len(sys.argv) < 5:
-        print('usage:   -n {1|2} -s {no|+1|gt}')
-        print('-n   1 for unigram, 2 for bigram')
-        print('-s  +1 for +1 smoothing, gt for Good-Touring, or no for no smoothing')
+        helpmsg()
         sys.exit(2)
 
     try:
         opts, args = getopt.getopt(argv,'n:s:',['smoothing='])
     except getopt.GetoptError:
-        print('usage:   -n {1|2} -s {no|+1|gt}')
-        print('-n   1 for unigram, 2 for bigram')
-        print('-s   +1 for +1 smoothing, gt for Good-Touring, or no for no smoothing')
+        helpmsg()
         sys.exit(2)
 
     for opt, arg in opts:
         if opt in ('-n'):
+            global n
             n = arg
         elif opt in ('-s', '--smoothing'):
+            global smoothing
             smoothing = arg
 
     print('n = ',n)
-    print('smoothing = ',smoothing)
+    print('smoothing = ',smoothing, '\n')
+
+    buildCounts()
+    print(counts)
+    print(len(counts))
+
+    buildUnigram()
+    print(unigram)
+
+def helpmsg():
+    print('usage:   -n {1|2} -s {no|+1|gt}')
+    print('-n   1 for unigram, 2 for bigram')
+    print('-s  +1 for +1 smoothing, gt for Good-Touring, or no for no smoothing')
+
 
 if __name__ == '__main__':
     main(sys.argv[1:])
